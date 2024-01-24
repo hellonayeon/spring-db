31744bc2cb8c9857b43b819cbdde47180c44607b
hellonayeon <hellonykwon@gmail.com>
2024-01-25 01:25:50 +0900

    JDBC 개발 - 조회

--- jdbc/src/test/java/me/hellonayeon/jdbc/connection/MemberRepositoryV0Test.java
+++ jdbc/src/test/java/me/hellonayeon/jdbc/connection/MemberRepositoryV0Test.java
@@ -1,10 +1,13 @@
 package me.hellonayeon.jdbc.connection;
 
 import java.sql.SQLException;
+import lombok.extern.slf4j.Slf4j;
 import me.hellonayeon.jdbc.domain.Member;
 import me.hellonayeon.jdbc.repository.MemberRepositoryV0;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+@Slf4j
 public class MemberRepositoryV0Test {
 
     MemberRepositoryV0 repository = new MemberRepositoryV0();
@@ -14,6 +17,11 @@ public class MemberRepositoryV0Test {
         // save
         Member member = new Member("memberV0", 10000);
         repository.save(member);
+
+        // findById
+        Member findMember = repository.findById(member.getMemberId());
+        log.info("findMember = {}", findMember);
+        Assertions.assertThat(findMember).isEqualTo(member);
     }
 
 }
