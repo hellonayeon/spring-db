42d06e10ac091d172c53c8aa7842dc4f973eb4af
hellonayeon <hellonykwon@gmail.com>
2024-01-25 01:32:47 +0900

    JDBC 개발 - 수정, 삭제

--- jdbc/src/test/java/me/hellonayeon/jdbc/connection/MemberRepositoryV0Test.java
+++ jdbc/src/test/java/me/hellonayeon/jdbc/connection/MemberRepositoryV0Test.java
@@ -1,6 +1,7 @@
 package me.hellonayeon.jdbc.connection;
 
 import java.sql.SQLException;
+import java.util.NoSuchElementException;
 import lombok.extern.slf4j.Slf4j;
 import me.hellonayeon.jdbc.domain.Member;
 import me.hellonayeon.jdbc.repository.MemberRepositoryV0;
@@ -22,6 +23,16 @@ public class MemberRepositoryV0Test {
         Member findMember = repository.findById(member.getMemberId());
         log.info("findMember = {}", findMember);
         Assertions.assertThat(findMember).isEqualTo(member);
+
+        // update: money 10000 -> 20000
+        repository.update(member.getMemberId(), 20000);
+        Member updateMember = repository.findById(member.getMemberId());
+        Assertions.assertThat(updateMember.getMoney()).isEqualTo(20000);
+
+        // delete
+        repository.delete(member.getMemberId());
+        Assertions.assertThatThrownBy(() -> repository.findById(member.getMemberId()))
+            .isInstanceOf(NoSuchElementException.class);
     }
 
 }
